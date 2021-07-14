5c3e9000abccf3fbca04dcb44a5b02959b26aff7
binghe_001 <1028386804@qq.com>
2021-07-14 21:43:29 +0800

    TCC分布式事务案例

--- tx-tcc/tx-tcc-bank02/src/main/java/io/transaction/tcc/bank02/service/impl/UserAccountBank02ServiceImpl.java
+++ tx-tcc/tx-tcc-bank02/src/main/java/io/transaction/tcc/bank02/service/impl/UserAccountBank02ServiceImpl.java
@@ -22,7 +22,6 @@ import io.transaction.tcc.common.entity.UserAccount;
 import io.transaction.tcc.common.mapper.UserAccountMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.dromara.hmily.annotation.HmilyTCC;
-import org.dromara.hmily.core.context.HmilyContextHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
